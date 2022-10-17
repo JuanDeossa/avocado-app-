4d1dfb501457c780925fde6afdb1f5d6a49cb5f0
JuanDeossa <JuanDeossa@users.noreply.github.com>
2022-10-17 23:42:48 +0000

    deploy: 93db4678a5bc25ccac1d00bc5d2eb1d15fd4abd9

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,17 +1,51 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+//base url for avocados API
+const BASE_URL =  "https://platzi-avo.vercel.app"
 
-const apiUrl =  "https://platzi-avo.vercel.app/api/avo"
+function formatPrice(price) {
+    const formatedPrice = new Intl.NumberFormat('en-US',{
+        style: 'currency',
+        currency: 'USD'
+    }).format(price)
+    return formatedPrice
+}
+//Click event function
+function cardClicked(e) {
+    if(e.target.nodeName==="H2"){
+        console.log(e.target.textContent);
+    }
+}
+
+//Async await query
 async function getData(path) {
     try {
         const response=await fetch(path)
         const {data}=await response.json()
-        console.log(data);
+        const array =[]
+        data.forEach(avo => {
+            //Destructuring data
+            const {name,price,image}=avo
+            //properties container
+            const container=document.createElement("div")
+            //property:image
+            const img=document.createElement("img")
+            img.src=`${BASE_URL}${image}`
+            //property:title
+            const h2=document.createElement("h2")
+            h2.textContent=name
+            //property:price
+            const div=document.createElement("div")
+            div.textContent=formatPrice(price)
+            container.append(img,h2,div)
+            array.push(container)
+        });
+        // Avocados container
+        container.append(...array)
+        container.addEventListener("click",cardClicked)
     } catch (error) {
         throw new Error(error)
     }
 }
+getData(`${BASE_URL}/api/avo`)
+/*
+*/
 
-getData(apiUrl)
